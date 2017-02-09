e49bdec828ea887e591e88b5229cbd4c10dfda9d
恵玉 李 <support@deviewstory.com>
2017-02-09 12:18:52 -0600

    fs http https

--- app.js
+++ app.js
@@ -1,28 +1,82 @@
-/*eslint-env node*/
+'use strict';
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
+const HOST = 'api.line.me'; 
+const PORT = process.env.PORT || 3000;
+const CH_ACCESS_TOKEN = process.env.TOKEN || 'rOUF+RwMgI16TO9EAOxm4qa2aPhol/REt0LkV9IvUSlWewd+pSFQ3c9Q/1BLk3fXoJsIhxvHiSn9sDiIeKtesjGiPi02vMx8VhIHtIy0PkoLM5kDdampTyc+WyaHi+hTHk6B4TH/LJL3PtcNV9srmAdB04t89/1O/w1cDnyilFU='; //Channel Access Tokenを指定
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+/**
+ * httpリクエスト部分
+ */
+const client = (messageId) => {    
+    let options = {
+        host: HOST,
+        port: 443,
+        path: `/v2/bot/message/${messageId}/content`,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Authorization': `Bearer ${CH_ACCESS_TOKEN}`,
+        }
+    };
 
-// create a new express server
-var app = express();
+    return new Promise((resolve, reject) => {
+        let req = https.request(options, (res) => {
+                    let buffers = [];
+                    res.on('data', (chunk) => {
+                        buffers.push(chunk);
+                    });
+                    res.on('end', () => {
+                        resolve(Buffer.concat(buffers));
+                    });
+        });
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+        req.on('error', (e) => {
+            reject(e);
+        });
+        req.end();
+    });
+};
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+http.createServer((req, res) => {    
+    if(req.url !== '/' || req.method !== 'POST'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('hello');
+    }
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    });        
+    req.on('end', () => {
+        if(body === ''){
+          console.log('bodyが空です。');
+          return;
+        }
+
+        let WebhookEventObject = JSON.parse(body).events[0];        
+        //メッセージが送られて来た場合
+        if(WebhookEventObject.type === 'message' && WebhookEventObject.message.type === 'image'){
+            client(WebhookEventObject.message.id)
+            .then((body)=>{
+                console.log(body);
+                fs.writeFile('./img.jpeg', body, 'utf-8', (err) => {
+                    if(err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log('成功');
+                });
+            },(e)=>{console.log(e)});
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('success');
+    });
+
+}).listen(PORT);
+
+console.log(`Server running at ${PORT}`);
